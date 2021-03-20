f29fc12b0de48d402bd554e8f46cca7a596c1d48
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2021-03-20 14:58:12 +0000

    Initial setup (#1)
    * feat: initial setup
    
    * feat: data folders to git ignore
    
    * feat: example uploader from flask docs
    
    * feat: working upload with file save and details save
    
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

---
+++ core.py
@@ -0,0 +1,18 @@
+from typing import Dict
+import os
+
+from werkzeug.datastructures import FileStorage
+
+import processes
+from interfaces import JSONFileDetailsRecorder, LocalFileSaver
+
+
+DETAILS_PATH = os.getenv("DETAILS_PATH", "")
+FILES_PATH = os.getenv("FILES_PATH", "")
+
+
+def save_file_and_record_details(file: FileStorage, filename: str, details: Dict[str, str]) -> None:
+    file_saver = LocalFileSaver(FILES_PATH)
+    details_recorder = JSONFileDetailsRecorder(save_path=DETAILS_PATH)
+    processes.save_file_and_record_details(file, filename, details, file_saver, details_recorder)
+    return
---
+++ web/app.py
@@ -0,0 +1,51 @@
+import sys
+
+from flask import Flask, flash, request, redirect, url_for, send_from_directory, render_template
+from werkzeug.utils import secure_filename
+
+sys.path.insert(0, "../")
+import core  # noqa E402
+
+
+UPLOAD_FOLDER = "../uploads"
+ALLOWED_EXTENSIONS = {"txt", "pdf", "png", "jpg", "jpeg", "gif"}
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+app.config["SECRET_KEY"] = 1234
+
+
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route("/", methods=["GET", "POST"])
+def upload_file():
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            flash("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == "":
+            flash("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            details = {}
+            for arg in ("category", "correspondenceType", "correspondenceDate"):
+                details[arg] = request.form[arg]
+            core.save_file_and_record_details(file, filename, details=details)
+            return redirect(url_for("upload_file"))
+
+    types = ("statement", "policy", "bill")
+    categories = ("insurance", "mortgage", "tv licence", "council tax", "electricity")
+    default_date = "2021-03"
+    return render_template("uploader.html", types=types, categories=categories, default_date=default_date)
+
+
+@app.route("/uploads/<filename>")
+def uploaded_file(filename):
+    return send_from_directory(app.config["UPLOAD_FOLDER"], filename)
