8a02164d1e295464ce7b3ebf72ec8b4eb19c9b0b
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2021-03-21 18:32:55 +0000

    Template overhaul (#6)
    * feat: bootstrap template
    
    * feat: call upload to storage transfer
    
    * feat: flash for task creation
    
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

--- web/app.py
+++ web/app.py
@@ -41,10 +41,13 @@ def upload_file():
             for arg in ("category", "correspondenceType", "correspondenceDate"):
                 details[arg] = request.form[arg]
             core.save_file_and_record_details(file, filename, details=details)
+            core.transfer_uploads_to_storage()
+            flash("File uploaded")
             return redirect(url_for("upload_file"))
 
     types = core.load_types()
     categories = core.load_categories()
+    # TODO update with current month
     default_date = "2021-03"
     return render_template("uploader.html", types=types, categories=categories, default_date=default_date)
 
@@ -57,10 +60,12 @@ def uploaded_file(filename):
 @app.route("/categories/create/<name>", methods=["GET"])
 def create_category(name):
     message = core.create_category(name)
-    return jsonify(message)
+    flash(message)
+    return redirect(url_for("upload_file"))
 
 
 @app.route("/types/create/<name>", methods=["GET"])
 def create_type(name):
     message = core.create_type(name)
-    return jsonify(message)
+    flash(message)
+    return redirect(url_for("upload_file"))
